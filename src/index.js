5b0527970984e0568c262f012d8c6038cd0f5c96
wraithjpr <james.p.r.wraith@gmail.com>
2018-04-21 17:15:27 +0100

    Implemented webpack.js.org/guides/getting-started/#creating-a-bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// import _ from 'lodash';
+const _ = require('lodash');
+
 function component () {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script.
+  element.innerHTML = _.join(['Hello', 'webpack... lodash imported.'], ' ');
 
   return element;
 }
