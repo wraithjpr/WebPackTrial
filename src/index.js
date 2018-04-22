70f01cca0c5ae98717f918058b4871eee8ba8f54
wraithjpr <james.p.r.wraith@gmail.com>
2018-04-22 18:06:32 +0100

    Implemented https://webpack.js.org/guides/asset-management/#loading-css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@
 // import _ from 'lodash';
 const _ = require('lodash');
 
+// import style sheet
+require('./style.css');
+
 function component () {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script.
   element.innerHTML = _.join(['Hello', 'webpack... lodash imported.'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
